7af17e2b607eab05cbd04f8188bd8f15447b99fd
Jeremy Ronquillo <j_ronquillo@u.pacific.edu>
2019-03-27 14:51:02 -0700

    mean normalization implemented

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import sys
 import pdb
 import re
 
-def main(argv, argc):
-    UNRATED = 99.0
+NUMBER_OF_JOKES = 100
+UNRATED = 99.0
 
+def main(argv, argc):
     if argv < 3:
         print("Usage: python main.py <jester-data>")
         exit(1)
 
     data = fileIO(argv)
+    meanNormalization(data)
 
     pdb.set_trace()
 
 
 def fileIO(argv):
+    print("-> fileIO()")
     DATA_FILE = 1
     data = []
     file = open(argv[DATA_FILE], 'r')
@@ -30,6 +33,35 @@ def fileIO(argv):
         data.append(tempDict)
     return data
 
+
+def isUnrated(rating):
+    return rating == UNRATED
+
+
+def meanNormalization(data):
+    print("-> meanNormalization()")
+
+    jokeRatingTotal = 0.0
+    jokeRatingAverage = 0.0
+
+    for joke in range(0, NUMBER_OF_JOKES):
+
+        jokeRatingCount = 0.0
+        jokeRatingTotal = 0.0
+        jokeRatingAverage = 0.0
+
+        for d in data:
+            rating = d["ratings"][joke]
+            if not isUnrated(rating):
+                jokeRatingTotal += rating
+                jokeRatingCount += 1
+
+        jokeRatingAverage = jokeRatingTotal / jokeRatingCount
+
+        for i in range(0, len(data)):
+            rating = data[i]["ratings"][joke]
+            if not isUnrated(rating):
+                data[i]["ratings"][joke] -= jokeRatingAverage
 
 if __name__ == "__main__":
     main(sys.argv, len(sys.argv))
